980a0f912018cf8886cf9d3f1c7dda8c1c37422f
hiroki-repo <hiroki7v11@gmail.com>
2024-05-06 13:55:06 +0900

    Update sse4a.cpp

--- np21_i386c/ia32/instructions/sse4a/sse4a.cpp
+++ np21_i386c/ia32/instructions/sse4a/sse4a.cpp
@@ -206,7 +206,7 @@ void SSE4a_INSERTQimm(void)
 	SSE_PART_GETDATA1DATA2_PD((double**)(&data1), (double**)(&data2), (double*)data2buf);
 	GET_PCBYTE((op2));
 	GET_PCBYTE((op3));
-	UINT64 bitrangedx = ((UINT64)((((SINT64)1) << (op2 & 63)) - ((SINT64)1)));
+	UINT64 bitrangedx = ((op2 & 63) == 0) ? ((UINT64)0xffffffffffffffff) : ((UINT64)((((SINT64)1) << (op2 & 63)) - ((SINT64)1)));
 	data1[0] = (data1[0] & (~(bitrangedx << (op3 & 63)))) | ((data2buf[0] & bitrangedx) << (op3 & 63));
 
 	TRACEOUT(("SSE4a_INSERTQimm"));
@@ -222,7 +222,7 @@ void SSE4a_INSERTQxmm(void)
 	SSE_PART_GETDATA1DATA2_PD((double**)(&data1), (double**)(&data2), (double*)data2buf);
 	op2 = ((data2buf[1] >> (8 * 0)) & 63);
 	op3 = ((data2buf[1] >> (8 * 1)) & 63);
-	UINT64 bitrangedx = ((UINT64)((((SINT64)1) << (op2 & 63)) - ((SINT64)1)));
+	UINT64 bitrangedx = ((op2 & 63) == 0) ? ((UINT64)0xffffffffffffffff) : ((UINT64)((((SINT64)1) << (op2 & 63)) - ((SINT64)1)));
 	data1[0] = (data1[0] & (~(bitrangedx << (op3 & 63)))) | ((data2buf[0] & bitrangedx) << (op3 & 63));
 
 	TRACEOUT(("SSE4a_INSERTQxmm"));
@@ -238,7 +238,7 @@ void SSE4a_EXTRQimm(void)
 	SSE_PART_GETDATA1DATA2_PD((double**)(&data1), (double**)(&data2), (double*)data2buf);
 	GET_PCBYTE((op2));
 	GET_PCBYTE((op3));
-	UINT64 bitrangedx = ((UINT64)((((SINT64)1) << (op2 & 63)) - ((SINT64)1)));
+	UINT64 bitrangedx = ((op2 & 63) == 0) ? ((UINT64)0xffffffffffffffff) : ((UINT64)((((SINT64)1) << (op2 & 63)) - ((SINT64)1)));
 	data1[0] = ((data1[0] >> (op3 & 63)) & bitrangedx);
 
 	TRACEOUT(("SSE4a_EXTRQimm"));
@@ -254,7 +254,7 @@ void SSE4a_EXTRQxmm(void)
 	SSE_PART_GETDATA1DATA2_PD((double**)(&data1), (double**)(&data2), (double*)data2buf);
 	op2 = ((data2buf[0] >> (8 * 0)) & 63);
 	op3 = ((data2buf[0] >> (8 * 1)) & 63);
-	UINT64 bitrangedx = ((UINT64)((((SINT64)1) << (op2 & 63)) - ((SINT64)1)));
+	UINT64 bitrangedx = ((op2 & 63) == 0) ? ((UINT64)0xffffffffffffffff) : ((UINT64)((((SINT64)1) << (op2 & 63)) - ((SINT64)1)));
 	data1[0] = ((data1[0] >> (op3 & 63)) & bitrangedx);
 
 	TRACEOUT(("SSE4a_EXTRQxmm"));
